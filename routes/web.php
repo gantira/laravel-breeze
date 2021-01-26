38932bf844a4772b1711c426e4737f96feb56952
Angga Gantira <angga.gantira@gmail.com>
2021-01-26 19:29:42 +0700

    4 Verifikasi Email Address

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::view('/about', 'about')->middleware(['auth'])->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
