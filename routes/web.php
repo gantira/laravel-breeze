421e4ca629312ec25c420befd28c9a3ee7c93b58
Angga Gantira <angga.gantira@gmail.com>
2021-01-26 16:18:15 +0700

    1 Apa Itu Laravel Breeze ?

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/about', 'about')->middleware(['auth'])->name('about');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
