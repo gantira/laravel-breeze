fef580128424a0efd217bebd4a6fdd599d31c0fe
Angga Gantira <angga.gantira@gmail.com>
2021-01-26 19:12:01 +0700

    3 Mari Kita Buat Komponen Untuk Error Nya

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,9 +10,9 @@
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        {{-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --}}
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('login') }}" novalidate>
             @csrf
 
             <!-- Email Address -->
@@ -20,6 +20,8 @@
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
+                <x-validation-message name="email" />
             </div>
 
             <!-- Password -->
@@ -30,6 +32,8 @@
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
+
+                <x-validation-message name="password" />
             </div>
 
             <!-- Remember Me -->
