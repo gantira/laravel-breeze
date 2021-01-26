5be9127c7f421f33bec97e47352e3dbe4801a2a7
Angga Gantira <angga.gantira@gmail.com>
2021-01-26 16:36:15 +0700

    2 Mari Kita Bongkar Sedikit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->middleware(['auth'])->name('home');
 Route::view('/about', 'about')->middleware(['auth'])->name('about');
 
 Route::get('/dashboard', function () {
